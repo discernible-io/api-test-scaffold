6badf02680207f1d1b6cfcdd3d77d648a272e60c
cableguard@cableguard.org <cableguard@cableguard.org>
2025-03-16 12:17:50 +0100

    TO BE FIXED HARDCODED VAR 2

--- src/middleware/vaultsetup-production.js
+++ src/middleware/vaultsetup-production.js
@@ -8,7 +8,7 @@ class ProductionVaultManager {
     this.vault.endpoint = config.get("VAULT_ENDPOINT");
     this.vault.apiVersion = "v1";
     this.roleId = config.get("VAULT_ROLE_CLIENTID");
-    this.secretId = config.get("VAULT_SECRET_SERVERID");
+    this.secretId = config.get("VAULT_SECRET_CLIENTID");
     this.renewalInterval = 60 * 60 * 1000; // 1 hour in milliseconds
   }
 
