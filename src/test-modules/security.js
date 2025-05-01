30c0ea701d265d72cc0f4f86cca00f68dc4d404a
cableguard@cableguard.org <cableguard@cableguard.org>
2025-05-01 19:47:54 +0200

    missed dependency  2

--- src/test-modules/security.js
+++ src/test-modules/security.js
@@ -3,6 +3,7 @@ const { fetchWithErrorHandling, stateManager } = require("../middleware/rodit");
 const { ulid } = require("ulid");
 const logger = require("../../config/logger");
 const nacl = require("tweetnacl");
+const { stateManager, fetchWithErrorHandling } = require("../middleware/rodit");
 
 // Standardized captureTestData function aligned with successful tests
 function captureTestData(testName, moduleName, result, testData) {
