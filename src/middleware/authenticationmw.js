ceb06c6ccb99d3f8315f6099303dd564b42c4ae1
cableguard@cableguard.org <cableguard@cableguard.org>
2025-05-21 13:28:03 +0200

    tuning aud checking  3

--- src/middleware/authenticationmw.js
+++ src/middleware/authenticationmw.js
@@ -6,12 +6,12 @@
 const { ulid } = require("ulid");
 const logger = require("../../config/logger");
 const nacl = require("tweetnacl");
+const { decodeJwt } = require("jose");
 // Import specific functions directly to avoid circular dependencies
 const { 
   validate_jwt_token_be,
   generate_jwt_token,
-  tokenService,
-  decodeJwt
+  tokenService
 } = require("../auth/tokenservice");
 const { 
   send_webhook,
