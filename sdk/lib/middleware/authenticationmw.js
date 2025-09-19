1c28e61ef4ac1ee9568567881f4e77ab828bcbe9
cableguard <cableguard@cableguard.org>
2025-09-19 15:09:32 +0000

    fix: rename rodit_id field to roditid in login portal request body

--- sdk/lib/middleware/authenticationmw.js
+++ sdk/lib/middleware/authenticationmw.js
@@ -1585,7 +1585,7 @@ async function login_portal(config_own_rodit, port) {
           "User-Agent": "RODiT-SDK",
         },
         body: JSON.stringify({
-          rodit_id: roditid,
+          roditid,
           timestamp,
           roditid_base64url_signature,
         }),
